4613092fffe0ba7aa3c0e9a053a430ed101e7604
Adrien Lemaire <dori@ikkyotech.com>
2014-08-03 20:17:13 +0900

    manipulate data

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,11 +1,35 @@
-var dataSet;
+var origSet,
+    dataSet = {};
+
+// 1. Fetch data
 $.getJSON('javascripts/kanji_learnt.json')
     .done(function (data) {
-        dataSet = data;
+        origSet = data;
     }
 )
 
 
+// 2. Manipulate data
+function addToSet(kana, kanji) {
+    if (!(kana in dataSet)) {
+        dataSet[kana] = [kanji];
+    }
+    if (kana in dataSet && !(kanji in dataSet[kana])) {
+        dataSet[kana].push(kanji);
+    }
+}
+
+$.map( origSet, function(el) {
+    for (i = 0; i < el.onyomi.length; i++) {
+        addToSet(el.onyomi[i], el.kanji);
+    }
+    for (i = 0; i < el.kunyomi.length; i++) {
+        addToSet(el.kunyomi[i], el.kanji);
+    }
+});
+
+
+// 3. Render data
 $(document).ready(function() {
     $('#main_content').html('<table cellpadding="0" cellspacing="0" border="0" class="display" id="kanji_learnt"></table>');
 
@@ -13,7 +37,7 @@ $(document).ready(function() {
         "data": dataSet,
         "columns": [
             { "title": "Kana" },
-            { "title": "Kanjis" }
+            { "title": "Kanji" }
         ]
     } );
 } )
