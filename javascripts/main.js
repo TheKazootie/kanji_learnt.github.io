8d1a182cc3ae8fa62d6736246dbe28557f3ed86e
Adrien Lemaire <dori@ikkyotech.com>
2014-08-04 01:43:50 +0900

    add columns per kanji

--- javascripts/main.js
+++ javascripts/main.js
@@ -16,7 +16,7 @@ function addToSet(kana, kanji) {
 $(function () {
     "use strict";
 
-    var i;
+    var i, dataReady, nbColumns, columns;
 
     $('#main_content').append('<table cellpadding="0" cellspacing="0" border="0" class="display" id="kanji_learnt"></table>');
 
@@ -31,18 +31,26 @@ $(function () {
                 addToSet(el.kunyomi[i], el.kanji);
             }
         });
+        dataReady = $.map(dataSet, function (values, key) {
+            return {"Kana": key, "Kanji": values};
+        });
+        nbColumns = Math.max.apply(
+            null,
+            Object.keys(data).map(function (key) {return data[key].length; })
+        );
+        columns = [
+            { "data": "Kana", "title": "Kana" }
+        ];
+        for (i = 0; i < nbColumns; i += 1) {
+            columns.push({"data": "Kanji", "title": "Kanji"});
+        }
 
         // 3. Display data
         $('#total_kanji').html(data.length);
         $('#kanji_learnt').dataTable({
             paging: false,
-            data: $.map(dataSet, function (values, key) {
-                return {"Kana": key, "Kanji": values};
-            }),
-            columns: [
-                { "data": "Kana", "title": "Kana" },
-                { "data": "Kanji", "title": "Kanji" }
-            ]
+            data: dataReady,
+            columns: columns
         });
     });
 });
