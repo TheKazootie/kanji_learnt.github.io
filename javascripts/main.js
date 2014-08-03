bcf8e07a4f97d473c4c3d244cb35e1789533376a
Adrien Lemaire <dori@ikkyotech.com>
2014-08-03 21:57:14 +0900

    move display in callback

--- javascripts/main.js
+++ javascripts/main.js
@@ -18,32 +18,29 @@ $(function () {
 
     var i;
 
-    // 1. Fetch data
-    $.getJSON('javascripts/kanji_learnt.json')
-        .done(function (data) {
-            // 2. Manipulate data
-            $.map(data, function (el) {
-                for (i = 0; i < el.onyomi.length; i += 1) {
-                    addToSet(el.onyomi[i], el.kanji);
-                }
-                for (i = 0; i < el.kunyomi.length; i += 1) {
-                    addToSet(el.kunyomi[i], el.kanji);
-                }
-            });
-
-        }
-    )
-
-
     $('#main_content').html('<table cellpadding="0" cellspacing="0" border="0" class="display" id="kanji_learnt"></table>');
 
-    $('#kanji_learnt').dataTable({
-        "data": $.map(dataSet, function (values, key) {
-            return {"Kana": key, "Kanji": values};
-        }),
-        "columns": [
-            { "data": "Kana" },
-            { "data": "Kanji" }
-        ]
+    // 1. Fetch data
+    $.getJSON('javascripts/kanji_learnt.json').done(function (data) {
+        // 2. Manipulate data
+        $.map(data, function (el) {
+            for (i = 0; i < el.onyomi.length; i += 1) {
+                addToSet(el.onyomi[i], el.kanji);
+            }
+            for (i = 0; i < el.kunyomi.length; i += 1) {
+                addToSet(el.kunyomi[i], el.kanji);
+            }
+        });
+
+        // 3. Display data
+        $('#kanji_learnt').dataTable({
+            "data": $.map(dataSet, function (values, key) {
+                return {"Kana": key, "Kanji": values};
+            }),
+            "columns": [
+                { "data": "Kana" },
+                { "data": "Kanji" }
+            ]
+        });
     });
-})
+});
