d49521e356454d1064652358d4348c015013ae21
Adrien Lemaire <dori@ikkyotech.com>
2014-08-05 23:13:58 +0900

    set global self invocated function

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,33 +1,26 @@
-var dataSet = {};
-
-function addToSet(kana, kanji) {
-    "use strict";
-
-    if (!(dataSet.hasOwnProperty(kana))) {
-        dataSet[kana] = [kanji];
-    }
-    if (dataSet.hasOwnProperty(kana) && (dataSet[kana].indexOf(kanji) === -1)) {
-        dataSet[kana].push(kanji);
-    }
-}
-
-function hiragana2katakana(text) {
-    "use strict";
-    var i, c, newText = "";
-
-    for (i = 0; i < text.length; i += 1) {
-        c = parseInt(text.charCodeAt(i) + 96, 10);
-        newText += String.fromCharCode("0x" + c.toString(16));
-    }
-    return newText;
-}
-
-
-// 3. Render data
 $(function () {
     "use strict";
+    var i, sortedData, dataSet = {};
 
-    var i, sortedData;
+    function addToSet(kana, kanji) {
+
+        if (!(dataSet.hasOwnProperty(kana))) {
+            dataSet[kana] = [kanji];
+        }
+        if (dataSet.hasOwnProperty(kana) && (dataSet[kana].indexOf(kanji) === -1)) {
+            dataSet[kana].push(kanji);
+        }
+    }
+
+    function hiragana2katakana(text) {
+        var i, c, newText = "";
+
+        for (i = 0; i < text.length; i += 1) {
+            c = parseInt(text.charCodeAt(i) + 96, 10);
+            newText += String.fromCharCode("0x" + c.toString(16));
+        }
+        return newText;
+    }
 
     $('#main_content').append('<table cellpadding="0" cellspacing="0" border="0" class="display" id="kanji_learnt"></table>');
 
